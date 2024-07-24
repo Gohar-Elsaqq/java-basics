a419938cd33e989cd0ca2399100c89cae82ce5a4
Gohar-Elsaqq <gohar.elsaqqa@gmail.com>
2024-07-24 21:07:58 +0300

    add class ForLoop

---
+++ src/main/java/com/gohar/ForLoop.java
@@ -0,0 +1,19 @@
+package com.gohar;
+
+public class ForLoop {
+
+
+        public static void main(String[] args) {
+            for(int i = 10; i >=0; i--) {
+                System.out.println("i = " + i);
+            }
+            System.out.println("----------for loop ----------");
+            for(int i = 0; i <10; i++) {
+                System.out.println("i = " + i);
+            }
+
+        }
+
+    }
+
+
