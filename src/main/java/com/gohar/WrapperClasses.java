3884c615158cdfce357561cc22a224ef75b4b9c9
Gohar-Elsaqq <gohar.elsaqqa@gmail.com>
2024-07-26 20:47:09 +0300

    add class WrapperClasses

---
+++ src/main/java/com/gohar/WrapperClasses.java
@@ -0,0 +1,12 @@
+package com.gohar;
+
+public class WrapperClasses {
+    public static void main(String[] args) {
+         Boolean a=true;
+         Character b='@';
+         Integer c =123;
+         Double d=123.456;
+         String e ="gohar";
+
+    }
+}
