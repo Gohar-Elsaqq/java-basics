b55ec84f0a8e69f4350b42f6163566d8985a594f
Gohar-Elsaqq <gohar.elsaqqa@gmail.com>
2024-07-26 21:31:34 +0300

    add class ArrayList2D add and remove and get index

--- src/main/java/com/gohar/ArrayList2D.java
+++ src/main/java/com/gohar/ArrayList2D.java
@@ -1,4 +1,34 @@
 package com.gohar;
 
+import java.util.ArrayList;
+
 public class ArrayList2D {
+    //2D arrayList  a=dynamic list of lists
+    //you can change the size of these lists during runtime
+    public static void main(String[] args) {
+
+        ArrayList<ArrayList<String>> grocery =new ArrayList();
+
+    ArrayList<String> bakery =new ArrayList();
+    bakery.add("pasta");
+    bakery.add("garlic bread");
+    bakery.add("donuts");
+
+        ArrayList<String> products =new ArrayList();
+        products.add("tomato");
+        products.add("zucchini");
+        products.add("peppers");
+
+        ArrayList<String> drank =new ArrayList();
+        drank.add("soda ");
+        drank.add("coffee");
+
+        grocery.add(bakery);
+        grocery.add(products);
+        grocery.add(drank);
+        System.out.println(grocery.get(0));
+        System.out.println(grocery.get(0).get(2));
+        System.out.println("removed array index 1 "+grocery.remove(1));
+
+}
 }
