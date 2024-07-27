2bb468a43fdc394a2b9121527ddad7dde7e733d0
Gohar-Elsaqq <gohar.elsaqqa@gmail.com>
2024-07-27 19:29:14 +0300

    add class Final

---
+++ src/main/java/com/gohar/Final.java
@@ -0,0 +1,10 @@
+package com.gohar;
+
+public class Final {
+    //final
+    public static void main(String[] args) {
+        final int a = 10;
+        //a = 20; // compile error = Cannot assign a value to final variable 'a'
+        System.out.println(a);
+    }
+}
