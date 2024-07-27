851a01aad47d2d9affe3ab5059cf4cb66ef00429
Gohar-Elsaqq <gohar.elsaqqa@gmail.com>
2024-07-27 19:38:14 +0300

    add main Object

---
+++ src/main/java/oop/Object.java
@@ -0,0 +1,9 @@
+package oop;
+
+public class Object {
+    //object = an instance of a class that may contain attributes and methods
+    //       and can be created using the 'new' keyword.
+    //       An object is created from a class when the class is instantiated.
+    // example  : (Phone ,desk ,cup)
+
+}
